3cc0536749f52faece7677fa45b82b3528d6237a
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-12-23 03:46:00 -0800

    Undo ESP8266 AAC huffman decoder hacks
    No need to only store hufftables at ints, we can access <32b ROM now.

--- src/libhelix-aac/sbrtabs.c
+++ src/libhelix-aac/sbrtabs.c
@@ -46,6 +46,8 @@
 
 #include "sbr.h"
 
+#define DPROGMEM __attribute__(( section(".data") ))
+
 /*  k0Tab[sampRateIdx][k] = k0 = startMin + offset(bs_start_freq) for given sample rate (4.6.18.3.2.1)
     downsampled (single-rate) SBR not currently supported
 */
@@ -98,7 +100,7 @@ const HuffInfo huffTabSBRInfo[10] PROGMEM = {
 };
 
 /* Huffman tables from appendix 4.A.6.1, includes offset of -LAV[i] for table i */
-const signed int /*short*/ huffTabSBR[604] PROGMEM = {
+const signed short huffTabSBR[604] PROGMEM = {
     /* SBR table sbr_tenv15 [121] (signed) */
     0,   -1,    1,   -2,    2,   -3,    3,   -4,    4,   -5,    5,   -6,    6,   -7,    7,   -8,
     -9,    8,  -10,    9,  -11,   10,  -12,  -13,   11,  -14,   12,  -15,  -16,   13,  -19,  -18,
