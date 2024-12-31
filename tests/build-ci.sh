047a8e6de4cb6553ebfded9457c9abfe2e5041ef
Earle F. Philhower, III <earlephilhower@yahoo.com>
2024-12-31 11:59:05 -0800

    Check CI fails

--- tests/build-ci.sh
+++ tests/build-ci.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "::group::Compiling $(basename $1) for $2 into $3"
-./arduino-cli compile -b "$2":opt=Optimize2 -v --warnings all --build-path "$3" "$1" 
+# Called by CI to compile and dump an example
+# built-ci.sh <FQBN> <output-directory> <full-path-to-ino>
+
+# Ensure any failure == exit with error
+set -e
+set -o pipefail
+
+echo "::group::Compiling $(basename $3) for $1 into $2"
+./arduino-cli compile -b "$1":opt=Optimize2 -v --warnings all --build-path "$2" "$3" || exit 255
 echo "::endgroup::"
-size "$3/$(basename $1).elf"
