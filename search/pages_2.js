0b04f0d383153b31ad3fe0248920a132dc2bc9ab
earlephilhower <earlephilhower@users.noreply.github.com>
2025-01-23 03:12:11 +0000

    Deploying to gh-pages from @ earlephilhower/BackgroundAudio@ed1140bc399a0c6dd2249777ff2c2414e611682b 🚀

--- search/pages_2.js
+++ search/pages_2.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['backgroundaudio_20play_20mp3_20aac_20and_20wav_20on_20the_20raspberry_20pi_20pico_20rp2040_20pico_202_20rp2350_20and_20esp32_0',['BackgroundAudio - Play MP3, AAC, and WAV on the Raspberry Pi Pico (RP2040), Pico 2 (RP2350), and ESP32',['../md_README.html',1,'']]]
+  ['backgroundaudio_0',['BackgroundAudio',['../index.html',1,'']]],
+  ['backgroundaudio_20play_20mp3_20aac_20and_20wav_20on_20the_20raspberry_20pi_20pico_20rp2040_20pico_202_20rp2350_20and_20esp32_1',['BackgroundAudio - Play MP3, AAC, and WAV on the Raspberry Pi Pico (RP2040), Pico 2 (RP2350), and ESP32',['../md_README.html',1,'']]]
 ];
