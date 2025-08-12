3caa6d8b2eba8da41820340c34f2b11812cdb759
earlephilhower <earlephilhower@yahoo.com>
2025-08-12 23:07:25 +0000

    Deploying to gh-pages from @ earlephilhower/BackgroundAudio@d1f4245c2771e7f16e7e0c418197aee95ddc570a 🚀

--- search/all_1.js
+++ search/all_1.js
@@ -2,7 +2,6 @@ var searchData=
 [
   ['_5fbackgroundtask_0',['_backgroundTask',['../classESP32I2SAudio.html#a0d338d0dfcf436bf5b1c8265c926409a',1,'ESP32I2SAudio']]],
   ['_5fonsent_1',['_onSent',['../classESP32I2SAudio.html#ab58dd1c2e51bf1216377c787d73c52d8',1,'ESP32I2SAudio']]],
-  ['_5fonsentcb_2',['_onSentCB',['../classESP32I2SAudio.html#a315f26b08af3a5be97d6eae2e104ecd6',1,'ESP32I2SAudio']]],
-  ['_5fonsentunder_3',['_onSentUnder',['../classESP32I2SAudio.html#acb06d412c2cc4af5f55721ab8f395476',1,'ESP32I2SAudio']]],
-  ['_5ftaskshim_4',['_taskShim',['../classESP32I2SAudio.html#a0a1a623a62e49d740e0dd579a3d6624a',1,'ESP32I2SAudio']]]
+  ['_5fonsentcb_2',['_onSentCB',['../classESP32I2SAudio.html#a854a4953c793dc725d4c6610e23985bb',1,'ESP32I2SAudio']]],
+  ['_5ftaskshim_3',['_taskShim',['../classESP32I2SAudio.html#a0a1a623a62e49d740e0dd579a3d6624a',1,'ESP32I2SAudio']]]
 ];
--- search/classes_1.js
+++ search/classes_1.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['esp32i2saudio_0',['ESP32I2SAudio',['../classESP32I2SAudio.html',1,'']]]
+  ['esp32i2saudio_0',['ESP32I2SAudio',['../classESP32I2SAudio.html',1,'']]],
+  ['esp32pdmaudio_1',['ESP32PDMAudio',['../classESP32PDMAudio.html',1,'']]]
 ];
